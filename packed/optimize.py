14b783ffc53ba39f3a462a426d4fc16a1be4f002
tomaz1502 <tomgm1502@gmail.com>
2023-08-01 17:09:19 -0300

    added script for removing unnecessary oleans

---
+++ packed/optimize.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import os
+
+def main():
+    total = 0
+    for _, _, xs in os.walk("./oleans"):
+        total += len(xs)
+    curr = 0
+    os.system("mkdir tmp")
+    for path, _, fileNames in os.walk("./oleans"):
+        for fileName in fileNames:
+            filePath = path + "/" + fileName
+            os.system("mv " + filePath + " tmp/")
+            ret = os.system("./process_lean_smt checkOleans.lean > /dev/null 2>&1")
+            if ret != 0:
+                os.system("mv tmp/" + fileName + " " + filePath)
+            curr += 1
+            print("done:", curr, "/", total)
+    os.system("rm -rf tmp")
+
+if __name__ == '__main__':
+    main()
