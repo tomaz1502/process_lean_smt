a4ef1d4e68d9065417db8f9be8616fdabe735c1c
tomaz1502 <tomgm1502@gmail.com>
2023-08-01 17:13:19 -0300

    added lean file with all tactics and theorems

---
+++ packed/checkOleans.lean
@@ -0,0 +1,17 @@
+open Smt.Reconstruction.Certifying
+
+#check notImplies1
+#check pull
+#check liftOrNToNeg
+#check liftOrNToImp
+#check smtSkolem
+#check resolution_1
+#check factor
+#check permutateOr
+#check trichotomy
+#check intTightLb
+#check intTightUb
+#check sumBounds
+#check arithMulNeg
+#check arithMulPos
+#check arithMulSign
