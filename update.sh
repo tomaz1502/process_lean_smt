5a9d15cbbf765c1f7d9f77b0e133b5f2885d9a49
tomaz1502 <tomgm1502@gmail.com>
2023-08-01 16:59:50 -0300

    update puts all oleans at packed

--- update.sh
+++ update.sh
@@ -6,4 +6,13 @@ lake update
 lake build
 cp lake-packages/smt/build/lib/Smt.olean packed/oleans
 cp lake-packages/smt/build/lib/Smt packed/oleans -r
+cp lake-packages/aesop/build/lib/Aesop.olean packed/oleans
+cp lake-packages/aesop/build/lib/Aesop packed/oleans -r
+cp lake-packages/mathlib/build/lib/Mathlib packed/oleans -r
+cp lake-packages/proofwidgets/build/lib/ProofWidgets.olean packed/oleans
+cp lake-packages/proofwidgets/build/lib/ProofWidgets packed/oleans -r
+cp lake-packages/Qq/build/lib/Qq.olean packed/oleans
+cp lake-packages/Qq/build/lib/Qq packed/oleans -r
+cp lake-packages/std/build/lib/Std.olean packed/oleans
+cp lake-packages/std/build/lib/Std packed/oleans -r
 cp build/bin/process_lean_smt packed
