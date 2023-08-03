0e1d1fbdd8613f9a745799b80dc47affc9d8c827
tomaz1502 <tomgm1502@gmail.com>
2023-08-03 11:41:22 -0300

    fix on optimize.py

--- packed/optimize.py
+++ packed/optimize.py
@@ -11,10 +11,10 @@ def main():
     for path, _, fileNames in os.walk("./oleans"):
         for fileName in fileNames:
             filePath = path + "/" + fileName
-            os.system("mv " + filePath + " tmp/")
+            os.system("mv " + filePath + " optimize_tmp/")
             ret = os.system("./process_lean_smt checkOleans.lean > /dev/null 2>&1")
             if ret != 0:
-                os.system("mv tmp/" + fileName + " " + filePath)
+                os.system("mv optimize_tmp/" + fileName + " " + filePath)
             curr += 1
             print("done:", curr, "/", total)
     os.system("rm -rf optimize_tmp")
